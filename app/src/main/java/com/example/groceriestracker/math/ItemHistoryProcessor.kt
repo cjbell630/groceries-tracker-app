50d666094457b414e21f37fbfd65df1726f3c6ba
Connor Bell <ronnoclleb@gmail.com>
2024-08-10 00:59:37 -0400

    add notes

--- app/src/main/java/com/example/groceriestracker/math/ItemHistoryProcessor.kt
+++ app/src/main/java/com/example/groceriestracker/math/ItemHistoryProcessor.kt
@@ -7,5 +7,9 @@ fun processStatusEvents(events: List<ItemStatus>) {
 }
 
 fun estimateTimeRemaining(events: List<ItemStatus>): Long {
+    // TODO get average curve from history
+    // TODO get current curve since last increase
+    // TODO predict future based on
+
     return 0 //TODO
 }
