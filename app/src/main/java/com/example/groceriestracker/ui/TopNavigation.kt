409575f4ac28ce9bf32312b90955f9a56772f319
Connor Bell <ronnoclleb@gmail.com>
2024-08-14 16:32:33 -0400

    add a bit of animation to pages

--- app/src/main/java/com/example/groceriestracker/ui/TopNavigation.kt
+++ app/src/main/java/com/example/groceriestracker/ui/TopNavigation.kt
@@ -1,16 +1,18 @@
 package com.example.groceriestracker.ui
 
+import androidx.compose.animation.*
+import androidx.compose.animation.core.EaseIn
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.navigation.NavBackStackEntry
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.groceriestracker.database.UpcAssociation
-import com.example.groceriestracker.repository.ItemRepository
 import com.example.groceriestracker.repository.ProcessedItem
-import com.example.groceriestracker.repository.UpcAssociationRepository
 import com.example.groceriestracker.ui.check.CheckScreen
 import com.example.groceriestracker.ui.components.BottomNavBar
 import com.example.groceriestracker.ui.components.DynamicFab
@@ -38,13 +40,39 @@ fun TopNavHost(
     incrementItemQuantity: (Int, Double) -> Unit,
     searchItems: (String) -> List<ProcessedItem>
 ) {
+    fun baseNavEnterTransition(): AnimatedContentTransitionScope<NavBackStackEntry>.() -> EnterTransition? {
+        return {
+            fadeIn(
+                animationSpec = tween(300, easing = LinearEasing)
+            ) + slideIntoContainer(
+                animationSpec = tween(100, easing = EaseIn),
+                towards = AnimatedContentTransitionScope.SlideDirection.Up
+            )
+        }
+    }
+    fun baseNavExitTransition(): AnimatedContentTransitionScope<NavBackStackEntry>.() -> ExitTransition? {
+        return {
+            fadeOut(
+                animationSpec= tween(300, easing = LinearEasing)
+            )
+        }
+    }
+
     NavHost(navController, startDestination = TopLevelDestinations.HOME_ROUTE) {
-        composable(TopLevelDestinations.HOME_ROUTE) {
+        composable(
+            TopLevelDestinations.HOME_ROUTE,
+            enterTransition = baseNavEnterTransition(),
+            exitTransition = baseNavExitTransition()
+        ) {
             floatingActionButton.show = true
             HomeScreen(innerPadding, allItems)
         }
 
-        composable(TopLevelDestinations.CHECK_ROUTE) {
+        composable(
+            TopLevelDestinations.CHECK_ROUTE,
+            enterTransition = baseNavEnterTransition(),
+            exitTransition = baseNavExitTransition()
+        ) {
             floatingActionButton.show = false
             CheckScreen(innerPadding, allItems, getUpcAssociation, addUpcAssociation, incrementItemQuantity, searchItems)
         }
