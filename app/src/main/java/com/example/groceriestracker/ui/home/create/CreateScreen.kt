2bcaa1914d667422856214d5b89da96c197f8bff
Connor Bell <ronnoclleb@gmail.com>
2024-08-18 14:10:12 -0400

    on click effect for create screen fab

--- app/src/main/java/com/example/groceriestracker/ui/home/create/CreateScreen.kt
+++ app/src/main/java/com/example/groceriestracker/ui/home/create/CreateScreen.kt
@@ -3,6 +3,7 @@ package com.example.groceriestracker.ui.home.create
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -10,10 +11,16 @@ import com.example.groceriestracker.models.Preset
 import com.example.groceriestracker.ui.components.AutofillTextField
 
 @Composable
-fun CreateScreen() {
+fun CreateScreen(setOnClick: (() -> Unit) -> Unit) {
     // TODO headers and visuals and stuff
     Column() {
+        // TODO change this to search bar?
         val autofillTextField = AutofillTextField<Preset>(Preset::searchByAlias) { preset -> preset?.name ?: "null" }
+        setOnClick {
+            if (autofillTextField.isValid()) {
+                /*TODO continue to next screen*/
+            }
+        }
         autofillTextField.Display(modifier = Modifier.padding(horizontal = 16.dp))
     }
 }
