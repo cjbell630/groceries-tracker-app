2c074be470a7ec7ba7decd74682c0c28473ac101
Connor Bell <ronnoclleb@gmail.com>
2024-08-23 16:50:34 -0400

    extract strings

--- app/src/main/java/com/example/groceriestracker/ui/pages/settings/SettingsHome.kt
+++ app/src/main/java/com/example/groceriestracker/ui/pages/settings/SettingsHome.kt
@@ -5,9 +5,10 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import com.example.groceriestracker.R
 import com.example.groceriestracker.repository.SettingsRepository
 import com.example.groceriestracker.repository.SettingsRepository.Settings.UseDynamic
 import com.example.groceriestracker.ui.components.SettingsCard
@@ -30,11 +31,14 @@ fun SettingsHome(settingsRepo: SettingsRepository) {
     }
     Column() {
         Text(
-            text = "UI",
+            text = stringResource(R.string.settings_category_ui),
             style = GroceriesTrackerTypography.headlineMedium,
             modifier = Modifier.padding(horizontal = 16.dp, vertical = 4.dp)
         )
-        SettingsCard(name = "Use Dynamic Theme", description = "Use theme based on app wallpaper") { modifier ->
+        SettingsCard(
+            name = stringResource(R.string.settings_name_dynamic_theme),
+            description = stringResource(R.string.settings_desc_dynamic_theme)
+        ) { modifier ->
             // TODO theme doesn't change until app restart, look at how it was done in NowInAndroid
             Switch(
                 modifier = modifier,
