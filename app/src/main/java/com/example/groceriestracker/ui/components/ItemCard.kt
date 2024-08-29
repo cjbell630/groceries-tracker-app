652d971b2968a61e5064a8cc68f00c0e20f41b34
Connor Bell <ronnoclleb@gmail.com>
2024-08-29 18:21:04 -0400

    update visualization

--- app/src/main/java/com/example/groceriestracker/ui/components/ItemCard.kt
+++ app/src/main/java/com/example/groceriestracker/ui/components/ItemCard.kt
@@ -1,6 +1,9 @@
 package com.example.groceriestracker.ui.components
 
+import android.util.Log
 import androidx.compose.foundation.layout.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.QuestionMark
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -11,6 +14,8 @@ import androidx.compose.ui.unit.sp
 import androidx.compose.ui.unit.times
 import com.example.groceriestracker.models.ProcessedItem
 import java.util.*
+import kotlin.math.floor
+import kotlin.math.roundToInt
 
 val THINGY = 8.dp
 val THINGY_SP = 8.sp
@@ -46,20 +51,34 @@ fun ItemCard(item: ProcessedItem) {
                 horizontalArrangement = Arrangement.End,
                 verticalAlignment = Alignment.CenterVertically // Centers items vertically
             ) {
+                if (item.history.estimatedExpiryTime == null) {
+                    Icon(
+                        Icons.Default.QuestionMark,
+                        contentDescription = "cannot calculate remaining time",
+                        modifier = Modifier.padding(end = 2 * THINGY)
+                    )
+                } else {
+                    // TODO verify this works
+                    // TODO add error checking for if lastUpdate is null ( should never happen but still)
+                    // use elvis
+                    Log.d("ItemCard",
+                        "${item.name}: item.history.estimatedExpiryTime: ${item.history.estimatedExpiryTime}\n" +
+                                "item.history.lastIncrease?.time!!: ${item.history.lastIncrease?.time!!}\n" +
+                                "Date().time: ${Date().time}"
+                    )
+                    val daysRemaining =( item.history.estimatedExpiryTime - Date().time).toDouble()/86400000.0
 
-                CircularProgressIndicator(
-                    progress = {
-                        // TODO verify this works
-                        // TODO add error checking for if lastUpdate is null ( should never happen but still)
-                        // use elvis
-                        ((Date().time - item.history.lastUpdate?.time!!) /
-                                (item.history.estimatedExpiryTime - item.history.lastUpdate?.time!!)
-                                ).toFloat()
-                    },
-                    color = Color.Red,
-                    trackColor = Color.Green,
-                    modifier = Modifier.padding(end = 2 * THINGY)
-                )
+                    val estimatedProductLife:Double = (item.history.estimatedExpiryTime - item.history.lastIncrease?.time!!).toDouble()
+                    val timeSinceLastIncrease: Double = (Date().time - item.history.lastIncrease?.time!!).toDouble()
+                    val progress = ( timeSinceLastIncrease/estimatedProductLife).toFloat()
+                    Text("${floor(daysRemaining)} days")
+                    CircularProgressIndicator(
+                        progress = { progress },
+                        color = Color.Red,
+                        trackColor = Color.Green,
+                        modifier = Modifier.padding(end = 2 * THINGY)
+                    )
+                }
                 Column() {
                     Text(item.remainingAmount.toString())
                     Text(item.unit)
