d41a55d0342eaeae884bb599b65c22f773dd3b0a
Connor Bell <ronnoclleb@gmail.com>
2024-08-29 18:20:37 -0400

    implement item history logic

--- app/src/main/java/com/example/groceriestracker/math/ItemHistoryProcessor.kt
+++ app/src/main/java/com/example/groceriestracker/math/ItemHistoryProcessor.kt
@@ -1,30 +1,83 @@
 package com.example.groceriestracker.math
 
+import android.util.Log
 import com.example.groceriestracker.models.ItemStatus
 
-class ProcessedItemHistory(private val events: List<ItemStatus>){
+class ProcessedItemHistory(private val events: List<ItemStatus>) {
     val lastUpdate: ItemStatus?
         get() = events.lastOrNull()
-    val lastIncrease: ItemStatus?
-        get() = events.lastOrNull() // TODO
-    val estimatedExpiryTime:Long
-    init{
-        // TODO process
-        estimatedExpiryTime = 0 //TODO
+    var lastIncrease: ItemStatus? = null
+    val estimatedExpiryTime: Long?
+
+    init {
+        // get history and current curve since last increase
+        val (historicalCurves, currentCurve) = processStatusEvents()
+        estimatedExpiryTime = estimateExiryTime(historicalCurves, currentCurve)
     }
 
+    fun processStatusEvents(): Pair<List<Curve>, List<ItemStatus>> {
+        var prev: Double = Double.MAX_VALUE
+        var currentTimeline: MutableList<ItemStatus> = mutableListOf()
+        var curves: MutableList<Curve> = mutableListOf()
+        for (status in events) {
+            // TODO need to account for a few things I took notes on (such as not always reaching 0)
+            if (status.amount <= prev) { // if decrease
 
+            } else { // increase
+                lastIncrease = status // NOTE sets last increase
+                curves.add(normalizeStatusEvents(currentTimeline))
+                currentTimeline = mutableListOf()
+            }
+            currentTimeline.add(status)
+            prev = status.amount
+        }
+        for (curve in curves) {
+            Log.d("processStatusEvents", curve.points.joinToString { "(${it.x}, ${it.y})   " })
+        }
+        lastIncrease = currentTimeline.first()
+        return Pair(curves, currentTimeline)
+    }
 }
 
-fun processStatusEvents(events: List<ItemStatus>) {
-
+fun normalizeStatusEvents(events: List<ItemStatus>): Curve {
+    // TODO error checking for length
+    val first = events.first()
+    val last = events.last()
+    val timeRange = (last.time - first.time).toDouble()
+    val amountRange = first.amount - last.amount
+    return Curve(events.map {
+        Point((it.time - first.time) / timeRange, (it.amount - last.amount) / amountRange)
+    })
 }
 
-fun estimateTimeRemaining(events: List<ItemStatus>): Long {
-    //val interpolator: SplineInterpolator<Double> = SplineInterpolator()
-    // TODO get average curve from history
-    // TODO get current curve since last increase
-    // TODO predict future based on
+fun estimateExiryTime(historicalCurves: List<Curve>, currentCurve: List<ItemStatus>): Long? {
+    // get average curve from history
+    val average = getSimpleLinearAverage(historicalCurves)
+    Log.d("estimateExiryTime", "average curve: " + average.points.joinToString { "(${it.x}, ${it.y})   " })
 
-    return 0 //TODO
+    /*
+    * P = estimated percentage of time elapsed of product usage
+    * C = time of most recent (current) update
+    * B = time of first (beginning) update
+    * T = total estimated time of product usage (from start to finish)
+    * R = estimated time remaining from most recent update
+    * E = estimated expiry time (long)
+    * P * T = C - B
+    * T = (C - B) / P
+    * R = T - (C - B)
+    * E = B + T = C + R
+    * */
+    val currentAmountPercent = currentCurve.last().amount / currentCurve.first().amount
+    val estimatedTimePercent = interpolateXAtY(average, currentAmountPercent) ?: return null // P
+    val timeRange = (currentCurve.last().time - currentCurve.first().time).toDouble() // C - B
+    val estimatedTotalLife = timeRange / estimatedTimePercent // T
+    val estimatedExpiryTime: Long = currentCurve.first().time + estimatedTotalLife.toLong() // E
+    Log.d("estimateExiryTime",
+        "currentAmountPercent: ${currentAmountPercent}\n" +
+                "estimatedTimePercent: ${estimatedTimePercent}\n" +
+                "timeRange: ${timeRange}\n" +
+                "estimatedTotalLife: ${estimatedTotalLife}\n" +
+                "estimatedExpiryTime: ${estimatedExpiryTime}"
+    )
+    return estimatedExpiryTime
 }
